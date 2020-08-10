a9a98f45e24f25bccfb2ca662dd3e28b37edadfa
LZambra <Leonardo.Waldemar.Zambra@outlook.es>
2020-08-10 08:54:59 -0400

    Se limitan ingresos en formulario

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,16 @@
 from django import forms
-from datetime import datetime
+import datetime
 
 class TMCForm(forms.Form):
-    montoUF=forms.DecimalField(label="montoUF", required=True, decimal_places=2, localize=True, max_digits = 8, widget=forms.NumberInput(
+    hoy = datetime.datetime.now()
+    limite =  datetime.datetime(hoy.year, hoy.month, 14)    
+    montoUF=forms.DecimalField(label="montoUF", required=True, decimal_places=2, min_value=1, localize=True, max_digits = 8, widget=forms.NumberInput(
         attrs={'class':'input--style-5'}
     ),initial=0)
-    plazo=forms.IntegerField(label="plazo", required=True, widget=forms.NumberInput(
+    plazo=forms.IntegerField(label="plazo", required=True, min_value=1, widget=forms.NumberInput(
         attrs={'class':'input--style-5'}
     ),initial=0)
     fechaSolicitud=forms.DateField(label="fechaSolicitud",  required=True, widget=forms.DateInput(
         format = ('%d-%m-%Y'),
-        attrs={'class':'input--style-5', 'type':'date'}
-    ) , initial=datetime.now().strftime("%Y-%m-%d"))
+        attrs={'class':'input--style-5', 'type':'date', 'max':limite.strftime("%Y-%m-%d")}
+    ) , initial=hoy.strftime("%Y-%m-%d"))
